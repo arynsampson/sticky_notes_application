ff64aa0e106c4275046633868779b40a52facba9
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2026-01-21 13:45:03 +0200

    Delete sticky_notes directory

--- sticky_notes/notes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Note
-from .models import Owner
-# from .models import Topic
-
-# Register your models here.
-admin.site.register(Note)
-admin.site.register(Owner)
-# admin.site.register(Topic)
